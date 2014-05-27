70e8b78b4357b7665c2cc6a94d081cc31cc9ac95
Théo <theophile.plancke@ensimag.imag.fr>
2014-05-27 14:25:14 +0200

    run maj et ajout d'attributs a la fenetre

--- run.sh
+++ run.sh
@@ -1,5 +1,50 @@
 #!/bin/sh
-cd build
-cmake ..
-make
-./Fenetre
+#Script de compilation et de lancement du projet
+
+help(){
+  echo    "Utilisation: \n"
+  echo    "./run.sh [-h] [-c] [-r] [-e]"
+  echo    "l’ordre des options n'a pas d'importance -r et -e sont incompatibles"
+  echo    "h: help"
+  echo    "c: Compilation uniquement"
+  echo    "e: Execution uniquement"
+  echo    "r: reset : recréation des dossier necessaires à la compilation\n"
+  exit
+}
+
+while getopts :hcer opt
+do
+	case $opt in
+	h) #option "h" help
+	opt_H="true" ;;
+	c) #option "c" Compilation
+	opt_C="true" ;;
+	e) #option "e" Execution
+	opt_E="true" ;;
+	r) #option "r" reset
+	opt_R="true" ;;
+	*)
+	echo "Mauvais arguments";
+	help;
+	exit 1;;
+	esac
+done
+test "$opt_H" && help
+if test "$opt_R" = "true"
+then
+	rm -r build/
+	mkdir build
+fi
+
+if test "$opt_E" != "true"
+then
+	cd build
+	cmake ..
+	make
+	cd ..
+fi
+
+if test "$opt_C" != "true"
+then
+	./build/Gest-Art_App
+fi
--- src/Controler/Base/main.cpp
+++ src/Controler/Base/main.cpp
@@ -1,9 +1,15 @@
-#include <gtkmm.h>
+#include <gtkmm/main.h>
+#include <gtkmm/window.h>
 
 int main(int argc, char** argv)
 {
-    Gtk::Main app(argc, argv);
-    Gtk::Window fenetre;
-    Gtk::Main::run(fenetre);
-    return 0;
+	//Création d'un objet Main de l'espace de noms GTK
+	Gtk::Main app(argc, argv);
+	//Création d'un objet Window de l'espace de noms GTK
+	Gtk::Window fenetre;
+	fenetre.set_title("Gest-Art Application");
+	fenetre.set_icon_from_file("Images/icon.png");
+	//Appel du main avec ajout de la fenetre -> Boucle principale
+	Gtk::Main::run(fenetre);
+	return 0;
 }
