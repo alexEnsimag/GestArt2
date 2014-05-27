b927d4fb18a05cbc698c0bce2a437cef88fc7836
Théo <theophile.plancke@ensimag.imag.fr>
2014-05-27 12:19:04 +0200

    Structure done

---
+++ installer.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sudo apt-get install g++
+sudo apt-get install libgtkmm-3.0-dev
+sudo apt-get install cmake
---
+++ src/Controler/Base/main.cpp
@@ -0,0 +1,9 @@
+#include <gtkmm.h>
+
+int main(int argc, char** argv)
+{
+    Gtk::Main app(argc, argv);
+    Gtk::Window fenetre;
+    Gtk::Main::run(fenetre);
+    return 0;
+}
