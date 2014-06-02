987beff17c868c51320c6641b523912ee3411e24
mathilde <mathilde.andre7@orange.fr>
2014-06-02 18:24:34 +0200

    ajout fichier parser

---
+++ src/Controler/Kinect/Parser.hpp
@@ -0,0 +1,23 @@
+#ifndef DEF_PARSER
+#define DEF_PARSER
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#include <string>
+
+using namespace std;
+
+class Parser{
+	public:
+		int parse(string nomFichier);
+
+};
+
+
+#endif
+
+
+
