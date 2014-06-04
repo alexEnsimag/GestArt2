7ea54ef5a53943612496aa1fd94401dfa63f03bd
mathilde <mathilde.andre7@orange.fr>
2014-06-04 16:44:53 +0200

    ajout segment.hpp

---
+++ src/Model/OpenGL/Segment.hpp
@@ -0,0 +1,23 @@
+#ifndef DEF_SEGMENT_
+#define	DEF_SEGMENT_
+
+#include <vector>
+#include <stdio.h>
+#include "math_3d.h"
+
+#include "Mesh.hpp"
+
+class Segment{
+	public:
+	Vector3f *p1;
+	Vector3f *p2;
+	std::vector<Vertex> vertexAssocies;
+	Segment(Vector3f* a, Vector3f* b){
+		p1 = a;
+		p2=b;
+	}
+};
+
+
+#endif
+
