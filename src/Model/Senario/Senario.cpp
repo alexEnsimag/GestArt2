d98a61b3ee265d3f6e4e84408b1b2b9fdd0a8360
Théo <theophile.plancke@ensimag.imag.fr>
2014-06-10 10:43:49 +0200

    Senario wip

--- src/Model/Senario/Senario.cpp
+++ src/Model/Senario/Senario.cpp
@@ -4,3 +4,19 @@
 void Senario::addActivite(Activite a){
 	activites.push_back(a);
 }
+
+void Senario::enregistrer(){
+
+}
+
+void Senario::charger(string nomFichier){
+
+}
+
+void Senario::setName(string s){
+	name = s;
+}
+
+string Senario::getName(){
+	return name;
+}
