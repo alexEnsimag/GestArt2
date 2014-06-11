4c7c0759da8f2e489fd40141a6ff9ba2c2ac5f9a
mathilde <mathilde.andre7@orange.fr>
2014-06-11 10:58:45 +0200

    ok activite forme + compile

--- src/Model/Scenario/ActiviteForme.cpp
+++ src/Model/Scenario/ActiviteForme.cpp
@@ -1,4 +1,5 @@
 #include "ActiviteForme.hpp"
+#include "ReceiveClassLabel.hpp"
 
 using namespace std;
 
@@ -13,4 +14,33 @@ const vector<string> ActiviteForme::possibleParams = MakeVector();
 
 
 void ActiviteForme::launch(){  
+// faire une forme
+//	m->afficherMessage("faire un carre");
+ 	// lancer of
+	// osc
+	of = new Of();
+	of->lancementOf();
+ 	lancerOsc();
+	// osc
+ 	// processing
+	lancerOsc();
+}
+
+void ActiviteForme::lancerOsc(){
+	ExamplePacketListener listener;
+
+	UdpListeningReceiveSocket s(
+
+			IpEndpointName( IpEndpointName::ANY_ADDRESS, PORT ),
+
+			&listener );    
+
+	listener.setS(&s);
+	listener.setActivite(this);
+	s.Run();
+
+}
+
+void ActiviteForme::killOf(){
+	of->killOf();
 }
