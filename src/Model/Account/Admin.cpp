13269c1efacc4481089d8de8a088f59306e87ab7
Olfa Sejjil <olfa.sejjil@ensimag.grenoble-inp.fr>
2014-06-11 14:27:42 +0200

    les fenetre pour Activite number

--- src/Model/Account/Admin.cpp
+++ src/Model/Account/Admin.cpp
@@ -5,18 +5,31 @@
 Admin::Admin(string id, string mdp){ 
      identifiant = id;
      motDePasse = mdp;
-     gestFileName = ""; // TODO à rajouter le nom du fichier 
+     gestFileName = "gestFile.txt";
+     //Ouverture du fichier
+     ifstream fichier(gestFileName.c_str(), ios::in);
+     if(!fichier){
+         cout << "Erreur à l'ouverture du Fichier des gestes" << endl;
+     } else {
+         string name;
+         int number;
+         while(!fichier.eof()){
+             fichier >> name >> number;
+         }
+         gests.insert ( std::pair<int,string>(number,name) );
+     }
+
 }
 void Admin::addGest(string name, int number, bool addFile=false){
     gests.insert ( std::pair<int,string>(number,name) );
-	if(addFile) {
-	    	const char * space = " ";
-		const char * endLine = "\n";
-		ofstream gestFile(gestFileName.c_str(), ios::ate);
-		gestFile << number;
-		gestFile << space;
-		gestFile << name.c_str();
-		gestFile << endLine;
-		gestFile.close();
-	}
+    if(addFile) {
+        const char * space = " ";
+        const char * endLine = "\n";
+        ofstream gestFile(gestFileName.c_str(), ios::ate);
+        gestFile << number;
+        gestFile << space;
+        gestFile << name.c_str();
+        gestFile << endLine;
+        gestFile.close();
+    }
 }
