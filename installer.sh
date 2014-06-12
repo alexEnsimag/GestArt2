c25d6f8f9911ca44c14258e59a51d4df89198a6e
mathilde <mathilde.andre7@orange.fr>
2014-06-12 13:06:27 +0200

    ajout de l'installationd e libvlc

--- installer.sh
+++ installer.sh
@@ -5,3 +5,4 @@ sudo apt-get install libgtkmm-3.0-dev
 sudo apt-get install cmake
 sudo apt-get install libglew1.8 libglew-dev
 sudo apt-get install assimp-utils libassimp-dev
+sudo apt-get install libvlc-dev
