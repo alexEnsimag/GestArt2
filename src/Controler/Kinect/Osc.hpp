83d54fe543380bf150ca34a99f425869df8fb5cb
mathilde <mathilde.andre7@orange.fr>
2014-06-13 20:31:16 +0200

    ajout activiteTestMouv et Osc classes

---
+++ src/Controler/Kinect/Osc.hpp
@@ -0,0 +1,24 @@
+#ifndef DEF_OSC
+#define DEF_OSC
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../../Model/Scenario/Activite.hpp"
+#include "../../Model/Scenario/ActiviteTestMouv.hpp"
+
+
+
+
+class Osc{
+	public:
+		void lancerOsc(int classLabel, Activite *act);
+	private:
+};
+
+
+#endif
+
+
+
